1441f0b8cb4acf48078b030141f8151696bfd4a2
vivek3454 <vivekparde2411@gmail.com>
2025-01-31 21:07:40 +0530

    code updated

---
+++ lib/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+if (!MONGODB_URL) {
+    throw new Error("Please define a mongodb url in env file");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = {
+        connection: null,
+        promise: null
+    }
+}
+
+export async function connectToDatabas() {
+    if (cached.connection) {
+        return cached.connection;
+    }
+
+    if (!cached.promise) {
+        const options = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        }
+
+        cached.promise = mongoose
+            .connect(MONGODB_URL, options)
+            .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.connection = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;   
+    }
+
+    return cached.connection;
+}
